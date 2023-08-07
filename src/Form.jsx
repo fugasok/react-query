2c3dd9d83e136138adf9a71da2bf325590eb30b8
Tibor <tibor736@hotmail.com>
2023-08-07 18:04:43 +0300

    Added query mutatin and list rerender

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,26 +1,43 @@
 import { useState } from 'react';
+import customFetch from './utils';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+	const [newItemName, setNewItemName] = useState('');
+	const queryClient = useQueryClient();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h4>task bud</h4>
-      <div className='form-control'>
-        <input
-          type='text '
-          className='form-input'
-          value={newItemName}
-          onChange={(event) => setNewItemName(event.target.value)}
-        />
-        <button type='submit' className='btn'>
-          add task
-        </button>
-      </div>
-    </form>
-  );
+	const { mutate: createTask, isLoading } = useMutation({
+		mutationFn: (taskTitle) => customFetch.post('/', { title: taskTitle }),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['tasks'] });
+			toast.success('tasks added');
+			setNewItemName('');
+		},
+		onError: (error) => {
+			toast.error(error.response.data.msg);
+		},
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createTask(newItemName);
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>task bud</h4>
+			<div className='form-control'>
+				<input
+					type='text '
+					className='form-input'
+					value={newItemName}
+					onChange={(event) => setNewItemName(event.target.value)}
+				/>
+				<button type='submit' className='btn' disabled={isLoading}>
+					add task
+				</button>
+			</div>
+		</form>
+	);
 };
 export default Form;
