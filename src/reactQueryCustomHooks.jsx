24898fd1a95a5f7b1591a6e24bd60c51af15a395
Tibor <tibor736@hotmail.com>
2023-08-07 19:03:59 +0300

    refactoring. Added: editTask and deleteTask

--- src/reactQueryCustomHooks.jsx
+++ src/reactQueryCustomHooks.jsx
@@ -1,5 +1,4 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import customFetch from './utils';
 
 export const useFetchTasks = () => {
@@ -11,4 +10,34 @@ export const useFetchTasks = () => {
 		},
 	});
 	return { isLoading, isError, data };
+};
+
+export const useCreateTask = () => {
+	const queryClient = useQueryClient();
+};
+
+export const useEditTask = () => {
+	const queryClient = useQueryClient();
+	const { mutate: editTask } = useMutation({
+		mutationFn: ({ taskId, isDone }) => {
+			return customFetch.patch(`/${taskId}`, { isDone });
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['tasks'] });
+		},
+	});
+	return { editTask };
+};
+
+export const useDeleteTask = () => {
+	const queryClient = useQueryClient();
+	const { mutate: deleteTask, isLoading: deleteTaskLoading } = useMutation({
+		mutationFn: (taskId) => {
+			return customFetch.delete(`/${taskId}`);
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['tasks'] });
+		},
+	});
+	return { deleteTask, deleteTaskLoading };
 };
